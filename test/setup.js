9cf0d8bb5df03cfd61ec0b9d8d72dad1a5d0797b
Max Foo <max.foo@me.com>
2017-02-24 15:39:31 +0800

    Install sinon-chai

--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,6 @@
 import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme';
+import sinonChai from 'sinon-chai';
 
+chai.use(sinonChai);
 chai.use(chaiEnzyme()); // Note the invocation at the end
